bc6cb399d9bcccaf04bcdfd48d9ae8cd0a0a3dc8
LuizGustavoMFerreira <luizmisslin@hotmail.com>
2023-05-28 19:45:41 -0300

    second commit

--- database/migrations/2023_05_28_124844_create_produtos_table.php
+++ database/migrations/2023_05_28_124844_create_produtos_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
             $table->string('nome', 100);
-            $table->text('descricao')->nullable();
-            $table->integer('peso')->nullable();
+            $table->text('descricao')->nullnable();
+            $table->integer('peso')->nullnable();
             $table->float('preco_venda', 8, 2)->default(0.01);
             $table->integer('estoque_minimo')->default(1);
             $table->integer('estoque_maximo')->default(1);
